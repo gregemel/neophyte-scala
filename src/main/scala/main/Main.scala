cd2785196c661d7712c1797f1675a866affac9aa
Greg Emel <greg.emel@gmail.com>
2018-03-10 11:38:39 -0800

    fixed naming; can run either module

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,7 +1,9 @@
 package main
 
-class Main {
+object Main {
+
   def main (args: Array[String] ): Unit = {
 
+    println("I'm here!!!!")
   }
 }
--- test/scala/MainTest.scala
+++ test/scala/MainTest.scala
@@ -1,11 +1,10 @@
 import main.Main
 import org.scalatest.{FlatSpec, Matchers}
 
-/** @version created manually **/
 class MainTest extends FlatSpec with Matchers {
 
-  it should "allow Main to be Tested" in {
-    val main = new Main
-    true should be(true)
+  it should "be a valid object" in {
+    val target = Main
+    target should not be Nil
   }
 }
